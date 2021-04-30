5ac49017eada13f3d7457da3f0f39951524b1889
Valdnet <47037905+Valdnet@users.noreply.github.com>
2021-04-30 09:19:53 +0200

    l10n: Change to a capital letter

--- src/modules/activities/ActivitiesTable.vue
+++ src/modules/activities/ActivitiesTable.vue
@@ -25,7 +25,7 @@
 		<DataTable
 			:data="data"
 			:header="headerDefinition"
-			:entity-name="t('health', 'activity')"
+			:entity-name="t('health', 'Activity')"
 			@addItem="addItem"
 			@updateItem="updateItem"
 			@deleteItem="deleteItem" />
