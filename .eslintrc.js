d9838c7d68bbc6e0eb0417b8e0df68f03b2c0512
flost <flost-dev@mailbox.org>
2022-02-01 17:04:04 +0100

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
 	extends: [
-		'@nextcloud'
+		'@nextcloud',
 	],
-};
+	rules: {
+		// no ending html tag on a new line (was warn in "vue/strongly-recommended")
+		'vue/html-closing-bracket-newline': ['error', { multiline: 'always' }],
+		// do not require JSDoc comments
+		'jsdoc/require-jsdoc': 'off',
+	},
+}
